44a5ddea5351780b7934b445c92aa206d692636d
Vitória Dourado <vickawaii14@gmail.com>
2024-10-04 12:42:21 -0300

    :tada: init: Inicializando projeto e atualizando components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-9xl text-red-600'>Vite</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
