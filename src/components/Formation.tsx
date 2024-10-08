62df7f480ff5dc3e3dfd4053ff5128381a51e198
Vitória Dourado <vickawaii14@gmail.com>
2024-10-08 17:23:44 -0300

    :art: style: Centralizacao dos cards projeto e academicformation

--- src/components/Formation.tsx
+++ src/components/Formation.tsx
@@ -12,15 +12,15 @@ interface AcademicFormationProps {
 
 const AcademicFormation: React.FC<AcademicFormationProps> = ({ formations }) => {
   return (
-    <section className="my-20 px-4 md:px-10 bg-blue-100 py-16 flex justify-center items-center">
-      <div>
+    <section className="my-20 px-4 md:px-10 bg-blue-100 py-16">
+      <div className="w-full max-w-7xl mx-auto">
         <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-blue-800 mb-10 text-center">
           Formação Acadêmica
         </h2>
         <div className="flex justify-center">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-xs md:max-w-3xl lg:max-w-6xl">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
             {formations.map((formation) => (
-              <div key={formation.id} className="bg-white shadow-lg rounded-lg p-6 w-full">
+              <div key={formation.id} className="bg-white shadow-lg rounded-lg p-6 w-full max-w-xs mx-auto">
                 <h3 className="text-lg sm:text-xl font-semibold text-gray-800">{formation.degree}</h3>
                 <p className="text-sm sm:text-base text-gray-600">{formation.institution}</p>
                 <p className="text-sm text-gray-500 italic">{formation.period}</p>
--- src/components/Projetos.tsx
+++ src/components/Projetos.tsx
@@ -10,30 +10,32 @@ interface ProjetoProps {
 
 const Projeto: React.FC<ProjetoProps> = ({ projetos }) => {
   return (
-    <div className="flex flex-col items-center pb-20 px-4 sm:px-6 md:px-10"> {/* Padding horizontal adicionado */}
+    <div className="flex flex-col items-center pb-20 px-4 sm:px-6 md:px-10">
       <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-green-300 mb-10 text-center">
         Projetos
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center w-full">
-        {projetos.map((projeto, index) => (
-          <a 
-            key={index} 
-            href={projeto.githubLink} 
-            target="_blank" 
-            rel="noopener noreferrer" 
-            className="bg-gradient-to-r from-green-400 to-blue-500 p-6 rounded-lg shadow-lg hover:shadow-xl hover:from-green-500 hover:to-blue-600 transition-shadow duration-300 w-full max-w-sm"
-          >
-            <img 
-              src={projeto.image} 
-              alt={projeto.title} 
-              className="w-full h-auto object-cover rounded-lg mb-4 aspect-w-16 aspect-h-9"
-            />
-            <h3 className="text-2xl font-bold text-white mb-2">{projeto.title}</h3>
-            <div className="text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-full transition-colors duration-300 text-center">
-              Ver no GitHub
-            </div>
-          </a>
-        ))}
+      <div className="w-full flex justify-center"> 
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center max-w-7xl mx-auto">
+          {projetos.map((projeto, index) => (
+            <a 
+              key={index} 
+              href={projeto.githubLink} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="bg-gradient-to-r from-green-400 to-blue-500 p-6 rounded-lg shadow-lg hover:shadow-xl hover:from-green-500 hover:to-blue-600 transition-shadow duration-300 w-full max-w-sm"
+            >
+              <img 
+                src={projeto.image} 
+                alt={projeto.title} 
+                className="w-full h-auto object-cover rounded-lg mb-4 aspect-w-16 aspect-h-9"
+              />
+              <h3 className="text-2xl font-bold text-white mb-2">{projeto.title}</h3>
+              <div className="text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-full transition-colors duration-300 text-center">
+                Ver no GitHub
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
